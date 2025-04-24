005e8504d1db4d7d23e073ddbc684d3911904ebf
Hakim-56 <hakimhazlif@gmail.com>
2025-04-24 13:53:21 +0100

    Delete the current user

--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -59,6 +64,12 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+// /^find/ is like to say all the methods that start with find, such as find, findOne, and findById etc.
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 
