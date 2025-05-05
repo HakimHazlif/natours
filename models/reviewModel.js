91a88e3ad831170bd452f9421941a30bcec32e9c
Hakim-56 <hakimhazlif@gmail.com>
2025-05-05 10:51:28 +0100

    Calculating aaverage rating on tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -39,6 +40,48 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRatig: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats.length > 0)
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingAverage: stats[0].avgRatig,
+      ratingQuantity: stats[0].nRating,
+    });
+  else
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingAverage: 0,
+      ratingQuantity: 4.5,
+    });
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// findByIdAndUpdate +  findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.clone().findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne() does NOT work here, the query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
