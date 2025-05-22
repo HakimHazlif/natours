37d626fcc531edde178215e949116d5936a0cd51
Hakim-56 <hakimhazlif@gmail.com>
2025-05-22 12:22:07 +0100

    Including a map with Mapbox: I can't use mapbox account, so i desided to skip this lecture

---
+++ public/js/mapbox.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+// const { contains } = require('validator');
+
+// const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+// console.log(locations);
+
+// delete L.Icon.Default.prototype._getIconUrl;
+
+// L.Icon.Default.mergeOptions({
+//   iconUrl: '/img/leaflet/marker-icon.png',
+//   shadowUrl: '/img/leaflet/marker-shadow.png',
+// });
+
+// const map = L.map('map').setView([33.5731, -7.5898], 12);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; OpenStreetMap contributors',
+// }).addTo(map);
+
+// L.marker([33.5731, -7.5898]).addTo(map).bindPopup('Casablanca').openPopup();
