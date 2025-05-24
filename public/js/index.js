f930a7f0e05e5a540e80605ed24369b03f29e974
Hakim-56 <hakimhazlif@gmail.com>
2025-05-24 19:50:20 +0100

    Updating user data with our api

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 // import { displayMap } from './mapbox';
 
 // const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutForm = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
+// this worked well but I didn't know how to protect my mapTiler token
 // if (mapBox) {
 //   const locations = JSON.parse(mapBox.dataset.locations);
 //   displayMap(locations);
@@ -23,3 +26,13 @@ if (loginForm)
   });
 
 if (logoutForm) logoutForm.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    updateData(name, email);
+  });
