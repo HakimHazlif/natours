6efa9bd63c9ec0cd58d2f4f97062539e907da7b7
Hakim-56 <hakimhazlif@gmail.com>
2025-05-24 20:19:30 +0100

    Updating user password with our API

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -3,19 +3,19 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const updateData = async (name, email) => {
+// type is either password or data
+export const updateSettings = async (data, type) => {
   try {
+    const urlParam = type === 'password' ? 'updateMyPassword' : 'updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url: `http://localhost:8000/api/v1/users/${urlParam}`,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
