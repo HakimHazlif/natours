b09e2c0168886ebdf2c0ffb47f6310f97021f998
HakimHazlif <hakimhazlif@gmail.com>
2025-05-26 14:37:43 +0100

    Integrating Stripe into the backend

---
+++ controllers/bookingController.js
@@ -0,0 +1,39 @@
+const Stripe = require('stripe');
+const Tour = require('../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1. get the currently booked tour
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const tour = await Tour.findById(req.params.tourID);
+
+  // 2. create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    mode: 'payment',
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+          unit_amount: tour.price * 100,
+        },
+        quantity: 1,
+      },
+    ],
+  });
+
+  // 3. create session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
